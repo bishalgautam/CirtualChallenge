3ec0348703f2774debfb018ec9f40ac1a250f586
bishalgautam <bishal.htd04@gmail.com>
2017-03-28 13:54:23 -0400

    added the search box and the search by stroke by the first column

--- private/shared/footer.php
+++ private/shared/footer.php
@@ -3,9 +3,27 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
 <script type="text/javascript">
 
+    $("#search").on("keyup", function() {
+    var value = $(this).val();
 
+    $("table tr").each(function(index) {
+        if (index != 0) {
+
+            $row = $(this);
+
+            var id = $row.find("td:first").text();
+
+            if (id.indexOf(value) != 0) {
+                $(this).hide();
+            }
+            else {
+                $(this).show();
+            }
+        }
+    });
+})
 </script>
   </body>
-</html>
+</html>	
 
 <?php if(isset($db)) { db_close($db); } ?>
